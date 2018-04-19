ace0e9eda2ec100d9b4d24f8224be5c90ed8648f
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 13:51:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |call|
+  planeteer_calls(calls_long).any? do |call|
     call.length > 4
 end
 
